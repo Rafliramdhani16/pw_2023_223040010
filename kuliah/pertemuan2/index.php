1e1603052c38559bfdbe64e72c3766a965e3e0d7
Rafliramdhani16 <rafliramdhani773@gmail.com>
2023-02-19 16:42:53 +0700

    mengerjakan tugas PW ke 1

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,28 @@
+<?php 
+$nama = "rafli";
+$bulan = "agustus";
+$hari =  " hari jum/'at kita 'ujian'"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pertemuan 2</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo "Hello, $nama";
+        
+         ?>
+    </h1>
+    <p>
+        <?php 
+        echo "Pemograman Web , $hari";
+        ?>
+    </p>
+</body>
+</html>
